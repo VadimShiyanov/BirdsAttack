2bb17f1058c67ed7177fd5ea0d5cf6a57c76f496
ohmiu <156603949+ohmiu@users.noreply.github.com>
2024-08-10 19:09:12 +0300

    Update __init__.py

--- backend/__init__.py
+++ backend/__init__.py
@@ -6,6 +6,10 @@ import os
 SERVER_URL = "https://localhost:8000"
 
 def register(username, password):
+    if username == '' or password == '' or not username.strip() or not password.strip():
+        raise Exception("Логин и / или пароль не могут быть пустым значением")
+    if ';' in username or ';' in password:
+        raise Exception("Запрещается использование символа `;` в логине или пароле")
     phash = hashlib.sha256(password.encode()).hexdigest()
     answer = json.loads(requests.get(SERVER_URL + f"/reg/{username};{phash}", verify=False).text)
     if answer["success"] == True:
@@ -16,6 +20,10 @@ def register(username, password):
         return False
 
 def login(username, password):
+    if username == '' or password == '' or not username.strip() or not password.strip():
+        raise Exception("Логин и / или пароль не могут быть пустым значением")
+    if ';' in username or ';' in password:
+        raise Exception("Запрещается использование символа `;` в логине или пароле")
     phash = hashlib.sha256(password.encode()).hexdigest()
     answer = json.loads(requests.get(SERVER_URL + f"/login/{username};{phash}", verify=False).text)
     if answer["success"] == True:
@@ -62,3 +70,13 @@ def get_my_username():
 
 def all_scores():
     return json.loads(requests.get(SERVER_URL + "/scores", verify=False).text)
+
+def onetime_connection_check():
+    try:
+        response = requests.get(SERVER_URL, verify=False, timeout=5)
+        if response.ok:
+            return True
+        else:
+            return False
+    except requests.exceptions.RequestException as e:
+        return False

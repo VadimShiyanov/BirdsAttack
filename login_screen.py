9aa0ec0a84fdedc5485f39b5028a769581bee9b3
ohmiu <156603949+ohmiu@users.noreply.github.com>
2024-08-10 19:11:22 +0300

    Update login_screen.py

--- login_screen.py
+++ login_screen.py
@@ -23,8 +23,9 @@ def login_screen(screen):
     login_menu_error_code = (assets['login_menu_error_code'])
 
     def enter_log_error_code():
-        time.sleep(3)
         screen.blit(login_menu_error_code, (0, 0))
+        pygame.display.update()
+        time.sleep(3)
 
 
     login_screen_running = True
@@ -50,12 +51,18 @@ def login_screen(screen):
             if pygame.mouse.get_pressed()[0]:
                 password = password_input_box.text
                 username = login_input_box.text
-                auth = backend.login(username, password)
-                if auth == True:
-                    login_screen_running = False
-                    return 'menu'
-                else:
-                    enter_log_error_code()
+                try:
+                    auth = backend.login(username, password)
+                    can_continue = True
+                except:
+                    can_continue = False
+                    print("Тут должна быть картинка что логин или пароль не может быть пустым текстом и / или недолжен содержать знак ;")
+                if can_continue:
+                    if auth == True:
+                        login_screen_running = False
+                        return 'menu'
+                    else:
+                        enter_log_error_code()
 
         if button_back.draw(screen, mouse):
             if pygame.mouse.get_pressed()[0]:

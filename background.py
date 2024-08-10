aab0d40b1015528f105c84b2340846a2c728ef11
ohmiu <156603949+ohmiu@users.noreply.github.com>
2024-08-10 13:14:43 +0300

    Delete background.py

--- background.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-class Background:
-    def __init__(self, image, pos=(0, 0)):
-        self.image = image
-        self.pos = pos
-
-    def draw(self, screen):
-        screen.blit(self.image, self.pos)
